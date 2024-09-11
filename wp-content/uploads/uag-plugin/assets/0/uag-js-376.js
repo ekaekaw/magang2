f147316ed79ff3a18abdd3f240994b7c7d2e8857
ekaekaw <Ekasetiya568@gmail.com>
2024-09-11 19:00:16 +0700

    eka.w

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-376.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"8e6051ea","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":false,"afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-8e6051ea', 376 );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-93dfe43f' );
+});
+ });
